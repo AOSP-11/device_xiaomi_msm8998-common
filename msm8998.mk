7fce2512297c5714e141cd77acd48e037b989160
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2017-10-10 14:12:48 +0530

    msm8998-common: Enable Thermal HAL by default
    Enable Thermal HAL for msm8998. It adds Thermal HAL
    to manifest.xml and thermal service to PRODUCT_PACKAGES.
    
    Change-Id: Id5070101c7a91955c050a9c5c8520cfa43cc8186

--- msm8998.mk
+++ msm8998.mk
@@ -380,6 +380,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.msm8998
+
 # Wifi
 PRODUCT_PACKAGES += \
     ipacm \
