6f26fb76da2eb1b3f6cd163cca42d6262de20d4f
sbwml <984419930@qq.com>
2019-11-17 07:07:45 +0800

    msm8998-common: Build qti-telephony-utils

--- msm8998.mk
+++ msm8998.mk
@@ -350,7 +350,9 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     telephony-ext \
     qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
