873b6fdc062ed10bd0909cd298dc3e3bee1e2894
sbwml <984419930@qq.com>
2019-11-19 06:28:44 +0800

    msm8998-common: Disable BOARD_BUILD_SYSTEM_ROOT_IMAGE
    笑死隔离屋只二哈，二哈笑说这玩意在 Android 10 中已经没有意义

--- msm8998.mk
+++ msm8998.mk
@@ -162,11 +162,6 @@ PRODUCT_PACKAGES += \
     XiaomiDoze \
     XiaomiParts
 
-# Device mapper verity
-PRODUCT_SYSTEM_VERITY_PARTITION=/dev/block/bootdevice/by-name/system
-PRODUCT_VENDOR_VERITY_PARTITION=/dev/block/bootdevice/by-name/cust
-$(call inherit-product, build/target/product/verity.mk)
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
