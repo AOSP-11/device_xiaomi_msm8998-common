910ba6caeeb79117b228c6ce3898e0b094330fe1
sbwml <984419930@qq.com>
2020-01-04 21:06:30 +0800

    msm8998-common: config.fs: init: Add AID_BLUETOOTH for android.hardware.bluetooth@1.0-service-qti

--- config.fs
+++ config.fs
@@ -1,6 +1,12 @@
 [AID_VENDOR_QTI_DIAG]
 value:2901
 
+[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND NET_ADMIN
+
 [vendor/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
