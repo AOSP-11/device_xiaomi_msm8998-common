289c66c5ad6836a74f0eb2e5c371ec7fee26259f
sbwml <984419930@qq.com>
2020-02-05 01:39:21 +0800

    msm8998-common: move spectrum script to vendor
    * Solve the failure to initialize the scheduling parameters on the GSI system

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -56,7 +56,7 @@ LOCAL_MODULE       := init.spectrum.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.spectrum.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
