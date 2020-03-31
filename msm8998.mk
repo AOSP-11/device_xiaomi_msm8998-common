be4c8cf9a54961a68c603a647f1e33a00f261c08
sbwml <984419930@qq.com>
2020-03-31 19:39:47 +0800

    Revert "msm8998-common: Build QTI BT Stack"
    This reverts commit 6aa03e2d16a6bf4e52e8a57b09f31c146093e8ca.

--- msm8998.mk
+++ msm8998.mk
@@ -116,10 +116,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio@2.0-impl \
-    liba2dpoffload \
     libbthost_if \
-    libldacBT_dec \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor
+    libldacBT_dec
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
