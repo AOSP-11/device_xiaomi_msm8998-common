74c07c4d2388b9f1830b0d20117175a4053e5866
sbwml <984419930@qq.com>
2020-03-31 19:41:10 +0800

    Revert "msm8998-common: Build libldacBT_dec"
    This reverts commit f03efbf77a60dc0627148470e911100dd487f474.

--- msm8998.mk
+++ msm8998.mk
@@ -116,8 +116,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio@2.0-impl \
-    libbthost_if \
-    libldacBT_dec
+    libbthost_if
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
