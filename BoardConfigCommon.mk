666347391d34d89e6c49180698322a5bcb66a681
sbwml <984419930@qq.com>
2020-09-26 23:24:44 +0800

    msm8998-common: Allow building with copy headers

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,6 +20,8 @@ PLATFORM_PATH := device/xiaomi/msm8998-common
 
 TARGET_SPECIFIC_HEADER_PATH := $(PLATFORM_PATH)/include
 
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
