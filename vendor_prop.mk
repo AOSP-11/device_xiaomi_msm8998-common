f08d884b4183cac52c80a4d3624b98fdecdf8b5e
sbwml <984419930@qq.com>
2020-09-30 14:40:13 +0800

    msm8998-common: Cancel WeChat and Alipay force Camera Hal1
    * 安卓11上它会导致无法扫描二维码

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -112,7 +112,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vidc.enc.dcvs.extra-buff-count=2 \
     camera.shutter_sound.blacklist=com.android.camera \
     vendor.camera.aux.packagelist=com.android.camera,org.lineageos.snap \
-    vendor.camera.hal1.packagelist=com.tencent.mm,com.eg.android.AlipayGphone,com.android.camera \
+    vendor.camera.hal1.packagelist=com.android.camera \
     persist.camera.privapp.list=com.android.camera,com.google.android.GoogleCamera,org.lineageos.snap
 
 # Charger
