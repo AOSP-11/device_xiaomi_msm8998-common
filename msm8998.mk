7a3fc4e596a30d3c07d2bd11843b85d488b757fd
sbwml <984419930@qq.com>
2020-10-09 01:18:41 +0800

    msm8998-common: Build missing and 64-bit variants android.hardware.camera.{common,device,provider}
    * Fixs:
     E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.common@1.0.so" not found: needed by /system/product/lib64/lib-imsvideocodec.so in namespace classloader-namespace-shared
     E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.provider@2.4.so" not found: needed by /system/product/lib64/lib-imsvideocodec.so in namespace classloader-namespace-shared
     E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "vendor.qti.hardware.camera.device@1.0.so" not found: needed by /system/product/lib64/lib-imsvideocodec.so in namespace classloader-namespace-shared
     E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.device@3.4.so" not found: needed by /system/product/lib64/lib-imsvideocodec.so in namespace classloader-namespace-shared

--- msm8998.mk
+++ msm8998.mk
@@ -149,8 +149,14 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
+    android.hardware.camera.common@1.0 \
+    android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5
+
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.camera.device@1.0
 
 # Display
 PRODUCT_PACKAGES += \
