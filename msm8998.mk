ccc26aea05a8f4f6cd6022be86218acf7c82b4b6
sbwml <984419930@qq.com>
2020-12-06 14:19:27 +0800

    msm8998-common: Build missing 64-bit variants camera component libs for lib-imsvideocodec.so

--- msm8998.mk
+++ msm8998.mk
@@ -150,10 +150,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.common@1.0 \
-    android.hardware.camera.device@3.4 \
+    android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5
+    android.hardware.camera.provider@2.6
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0
