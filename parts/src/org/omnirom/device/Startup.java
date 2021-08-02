680872697cc2c2fe1e5333d737b8a128265d25c2
sbwml <984419930@qq.com>
2021-08-03 05:24:52 +0800

    Revert "msm8998-common: parts: Remove hw button settings"
    This reverts commit 5b6559fdaaff45215a704012f7de3906213ff4b1.

--- parts/src/org/omnirom/device/Startup.java
+++ parts/src/org/omnirom/device/Startup.java
@@ -25,6 +25,8 @@ import android.content.SharedPreferences;
 
 import androidx.preference.PreferenceManager;
 
+import org.omnirom.device.Preference.BacklightPreference;
+import org.omnirom.device.Preference.ButtonSwapPreference;
 import org.omnirom.device.Preference.FastChargePreference;
 import org.omnirom.device.Preference.S2SVibratorStrengthPreference;
 import org.omnirom.device.Preference.SweepToSleepPreference;
@@ -37,6 +39,8 @@ public final class Startup extends BroadcastReceiver {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
             SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
 
+            BacklightPreference.FEATURE.restore(sp);
+            ButtonSwapPreference.FEATURE.restore(sp);
             FastChargePreference.FEATURE.restore(sp);
             SweepToSleepPreference.FEATURE.restore(sp);
             S2SVibratorStrengthPreference.FEATURE.restore(sp);
